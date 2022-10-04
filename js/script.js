d1d04c5cbac8d63824d8bba0bb604048a3dbfae0
NAME <vandriev@bk.ru>
2022-10-05 01:26:26 +0300

    FirstWork

---
+++ js/script.js
@@ -0,0 +1,24 @@
+        // Функция ymaps.ready() будет вызвана, когда
+        // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+        ymaps.ready(init);
+        function init(){
+            // Создание карты.
+            var myMap = new ymaps.Map("map", {
+                // Координаты центра карты.
+                // Порядок по умолчанию: «широта, долгота».
+                // Чтобы не определять координаты центра карты вручную,
+                // воспользуйтесь инструментом Определение координат.
+                center: [48.872185, 2.354224],
+                // Уровень масштабирования. Допустимые значения:
+                // от 0 (весь мир) до 19.
+                zoom: 15
+            });
+
+var myPlacemark = new ymaps.Placemark([48.872185, 2.354224], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'Subtract.svg',
+    iconImageSize: [30, 42],
+    iconImageOffset: [-3, -42] ,
+});
+myMap.geoObjects.add(myPlacemark);
+        }
